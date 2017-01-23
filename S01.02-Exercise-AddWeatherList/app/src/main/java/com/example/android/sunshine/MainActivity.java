7540b962853eb38cc2e2082be97b07af8903b15a
Stefano Pernat <stefano.pernat@gmail.com>
2017-01-23 20:32:12 +0100

    TODO 3-4: Add Dummy data

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -20,8 +20,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    // TODO (1) Create a field to store the weather display TextView
     private TextView weatherTextView;
 
     @Override
@@ -29,11 +27,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
         weatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        String[] dummyData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C"
+        };
 
-        // TODO (3) Create an array of Strings that contain fake weather data
-
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String weatherDay: dummyData){
+            weatherTextView.setText(weatherTextView.getText()+weatherDay+"\n");
+        }
     }
 }
