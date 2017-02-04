3ffae3d46fdce3656e9d08959a9147fd389348a1
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-04 11:35:31 +0100

    TODO 24-32 Implemented methods inside ForecastAdapter

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.sunshine;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,17 +15,24 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     @Override
     public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.forecast_list_item, parent, false);
+        return new ForecastAdapterViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
-
+        holder.mWeatherTextView.setText(mWeatherData[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return (mWeatherData == null ? 0 : mWeatherData.length);
+    }
+
+    public void setWeatherData(String[] weatherData){
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
     }
 
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
