3a1754f8f10178ff1c9a34fd3305096ba24d193a
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-23 16:13:32 +0100

    TODO 5: Added Preference from resource

--- S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -15,6 +15,6 @@ import android.view.ViewGroup;
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-
+            addPreferencesFromResource(R.xml.pref_general);
     }
 }
