20269ae217bd1c083224f76629c244e2ff4ada5c
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-23 10:34:01 +0100

    TODO 2: DisplayHomeAsUpEnabled set to true

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -8,5 +8,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
