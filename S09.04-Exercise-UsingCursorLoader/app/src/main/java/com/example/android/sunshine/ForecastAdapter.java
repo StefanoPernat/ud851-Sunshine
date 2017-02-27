60358707e9a359f983c7fe31080b178d4b4ba997
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-27 19:11:02 +0100

    TODO 11,12: created method SwapCursor

--- S09.04-Exercise-UsingCursorLoader/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S09.04-Exercise-UsingCursorLoader/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -140,8 +140,10 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
         notifyDataSetChanged();
     }
 
-//  TODO (11) Create a new method that allows you to swap Cursors.
-//      TODO (12) After the new Cursor is set, call notifyDataSetChanged
+    public void swapCursor(Cursor cursor){
+        mCursor = cursor;
+        notifyDataSetChanged();
+    }
 
     /**
      * A ViewHolder is a required part of the pattern for RecyclerViews. It mostly behaves as
